fe40a8db35256dd900997b02b2cd15eb76a6c0ea
ysk <2496383135@qq.com>
2025-08-13 11:33:49 +0800

    dd

--- vue.config.js
+++ vue.config.js
@@ -23,7 +23,7 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
-  publicPath: process.env.NODE_ENV === 'production' ? '/hamster-chat.dns.army/' : '/',
+  publicPath: '/hamster-chat.dns.army/',
   outputDir: 'dist',
   assetsDir: 'static',
   lintOnSave: process.env.NODE_ENV === 'development',
